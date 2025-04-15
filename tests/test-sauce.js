f4b3fdf8aff42b8eb3a9800738ac7a7b81526f87
msuva <rahadiyansuva@gmail.com>
2025-04-15 20:45:51 +0700

    task sesi 10

--- tests/test-sauce.js
+++ tests/test-sauce.js
@@ -1,11 +1,16 @@
 const {Builder, By, until} = require('selenium-webdriver');
 const assert = require('assert');
+const chrome = require('selenium-webdriver/chrome');
+// const firefox = require('selenium-webdriver/firefox');
 
 describe('Login & Sort', function(){
     let driver;
+    
+    beforeEach(async function(){
+        options = new chrome.Options();
+        // options.addArguments("--headless");
 
-    it('Sukses Login', async function(){
-        driver = await new Builder().forBrowser('chrome').build();
+        driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
 
         await driver.get('http://www.saucedemo.com');
         const title = await driver.getTitle();
@@ -14,7 +19,7 @@ describe('Login & Sort', function(){
         assert.strictEqual(title, 'Swag Labs');
         
         //input
-        let inputUsername = await driver.findElement(By.xpath('//*[@id="user-name"]'))
+        let inputUsername= await driver.findElement(By.xpath('//*[@id="user-name"]'))
         let inputPassword = await driver.findElement(By.xpath('//*[@id="password"]'))
         let buttonLogin = await driver.findElement(By.xpath('//*[@id="login-button"]'))
         await inputUsername.sendKeys('standard_user')
@@ -24,20 +29,34 @@ describe('Login & Sort', function(){
         //assert: text dalam element benar
         let textAppLogo = await driver.findElement(By.className('app_logo'))
         let logotext = await textAppLogo.getText()
-        assert.strictEqual(logotext, 'Swag Labs')
-
-        //Urut produk Z-A
         let sortProduct = await driver.findElement(By.css('[data-test="product-sort-container"]'))
-        let sortText = await driver.findElement(By.css('[value="za"]'))
+        assert.strictEqual(logotext, 'Swag Labs')       
         await sortProduct.click()
+
+    });
+ 
+    it('Sort Z-A', async function(){ 
+        //Urut produk Z-A
+        let sortText = await driver.findElement(By.css('[value="za"]'))
         await sortText.click()
 
         let textActiveOption = await driver.findElement(By.className('active_option'))
         let optionText = await textActiveOption.getText()
-        assert.strictEqual(optionText, 'Name (Z to A)')
+        assert.strictEqual(optionText, 'Name (Z to A)')       
+    });
 
-        await driver.sleep(3000);
+    it('Sort Low-High', async function(){ 
+        //Urut produk Z-A
+        let sortText = await driver.findElement(By.css('[value="lohi"]'))
+        await sortText.click()
+
+        let textActiveOption = await driver.findElement(By.className('active_option'))
+        let optionText = await textActiveOption.getText()
+        assert.strictEqual(optionText, 'Price (low to high)')       
+    });
+
+    afterEach(async function(){
+        await driver.sleep(2000);
         await driver.quit();
     });
 });
-
